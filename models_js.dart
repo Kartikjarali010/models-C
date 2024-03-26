0eb9d4e66e499601b22894203aec14b29835198b
Kartik Jarali <147948942+Kartikjarali010@users.noreply.github.com>
2024-03-26 22:29:39 +0530

    Add files via upload

---
+++ models_js.dart
@@ -0,0 +1,177 @@
+import 'package:timepass/Models/name_model.dart';
+
+List<NamesModel> inpp = [
+  NamesModel(topic: " JavaScript Basics"),
+  NamesModel(topic: " JavaScript Control Flow Statements"),
+  NamesModel(topic: " JavaScript Loops"),
+  NamesModel(topic: " JavaScript Expression and Operators"),
+  NamesModel(topic: " JavaScript Objects"),
+  NamesModel(topic: " JavaScript Functions"),
+  NamesModel(topic: " JavaScript Regular Expression"),
+  NamesModel(topic: " JavaScript Events"),
+  NamesModel(topic: " JavaScript OOPs"),
+  NamesModel(topic: " JavaScript Inheritance and Prototype Chain"),
+  NamesModel(topic: " JavaScript Memory Management"),
+  NamesModel(topic: " JavaScript Promises"),
+  NamesModel(topic: " JavaScript Iterators and generators"),
+  NamesModel(topic: " JavaScript Validations"),
+  NamesModel(topic: "JavaScript Exception Handling"),
+  NamesModel(topic: "JavaScript Global Objects"),
+  NamesModel(topic: "JavaScript Miscellaneous"),
+// NamesModel(Topic: ""),       // (for upcoming topic......)
+];
+
+
+List<NamesModel> outt= [
+  NamesModel(descp:
+"1. Introduction to JavaScript\n"
+"2. JavaScript Code Execution\n"
+"3. Console in JavaScript\n"
+"4. JavaScript Versions\n"
+"5. JavaScript Syntax\n"
+"6. JavaScript Comments\n"
+"7. JavaScript data types\n"
+"8. JavaScript variables\n"
+"   -> let Keyword\n"
+"   -> var Keyword\n"
+"   -> const Keyword\n"),
+  NamesModel(descp:
+"1. JS return Statement\n"
+"2. JS break Statement\n"
+"3. JS continue Statement\n"
+"4. JS throw Statement\n"
+"5. JS if…else Statement\n"
+"6. JS switch Statement\n"
+"7. JS try…catch Statement\n"),
+  NamesModel(descp:
+"1. JS for Loop\n"
+"2. JS do…while Loop\n"
+"3. JS while Loop\n"
+"4. JS for…in Loop\n"
+"5. JS for…of Loop\n"
+"6. JS labeled Statement\n"
+"7. JS break Statement\n"
+"8. JS continue Statement\n"),
+  NamesModel(descp:
+"1. JS Assignment operators\n"
+"2. JS Comparison operators\n"
+"3. JS Arithmetic operators\n"
+"4. JS Bitwise operators\n"
+"5. JS Logical operators\n"
+"6. JS BigInt Operators\n"
+"7. JS String operators\n"
+"8. JS Ternary operator\n"
+"9. JS Comma operator\n"
+"10. JS Unary operators\n"
+"11. JS Relational operators\n"),
+  NamesModel(descp:
+"1. JS Array\n"
+"2. JS String\n"
+"3. JS Date\n"
+"4. JS Number\n"
+"5. JS Math\n"
+"6. JS Object\n"
+"7. JS Boolean\n"
+"8. JS JSON\n"
+"9. JS Map\n"
+"10. JS Set\n"
+"11. JS Atomics\n"
+"12. JS BigInt\n"
+"13. JS Promise\n"
+"14. JS Proxy\n"
+"15. JS Reflect\n"
+"16. JS WeakMap\n"
+"17. JS WeakSet\n"),
+  NamesModel(descp:
+"1. JS Functions\n"
+"2. JS Function Definitions\n"
+"3. JS Function Call\n"
+"4. JS Function Expression\n"
+"5. JS Pure Functions\n"
+"6. JS Function Parameters\n"
+"7. JS Function Invocation\n"
+"8. JS Anonymous Functions\n"
+"9. JS Arrow functions\n"
+"10. JS Nested functions\n"
+"11. JS Function Generator\n"
+"12. JS Function binding\n"
+"13. JS Async/Await Function\n"
+"14. Hoisting in JavaScript\n"),
+  NamesModel(descp:
+"1. JS Regular expressions\n"
+"2. JS RegExp [abc] Expression\n"
+"3. JS RegExp S Metacharacter\n"
+"4. JS RegExp m Modifier\n"
+"5. JS RegExp ?! Quantifier\n"
+"6. JS RegExp {X,Y} Quantifier\n"
+"7. JS RegExp test() Method\n"
+"8. JS RegExp [^0-9] Expression\n"),
+  NamesModel(descp:
+"1. JS Events\n"
+"2. JS onclick Event\n"
+"3. JS dblclick Event\n"
+"4. JS onload Event\n"
+"5. JS onresize Event\n"
+"6. JS onblur Event\n"
+"7. JS onchange Event\n"
+"8. JS Focus Event \n"
+"9. JS Clipboard Event\n"
+"10. JS onscroll Event\n"),
+  NamesModel(descp:
+"1. JS Classes In JavaScript\n"
+"2. JS class expression\n"
+"3. JS Object Constructors\n"
+"4. JS Static Methods\n"
+"5. JS Prototype\n"
+"6. JS Constructor Method\n"
+"7. JS Encapsulation\n"
+"8. JS Inheritance\n"
+"9. JS Polymorphism\n"
+"10. JS Abstraction\n"),
+  NamesModel(descp:
+"1. Prototype Inheritance in JavaScript \n"
+"2. Prototype Chain in JavaScript\n"
+"3. JS Object Constructors\n"
+"4. JS Prototype in JavaScript\n"),
+  NamesModel(descp:
+"1. JS Memory Management\n"
+"2. JS Garbage Collection\n"),
+  NamesModel(descp:
+"1. JS Promise\n"
+"2. JS Promise Chaining\n"
+"3. JS Errors Throw and Try to Catch\n"
+"4. JS Class compositions in JavaScript\n"),
+  NamesModel(descp:
+"1. JS Iterator\n"
+"2. JS Function Generator \n"),
+  NamesModel(descp:
+"1. JS Form Validation\n"
+"2. JS Email Validation\n"
+),
+  NamesModel(descp:
+"1. JS Exception Handling\n"
+"2. JS try-catch Statement\n"
+"3. JS Promises\n"
+"4. JS async/await\n"),
+  NamesModel(descp:
+"1. JS encodeURI(), encodeURIComponent() and decodeURIComponent() Method\n"
+"2. JS eval() Method\n"
+"3. JS globalThis Property\n"
+"4. JS Global property\n"
+"5. JS Infinity\n"
+"6. JS Undefined vs Undeclared\n"),
+  NamesModel(descp:
+"1. JS this Keyword\n"
+"2. JS Strict Mode\n"
+"3. JS setTimeOut() and setInterval() Method\n"
+"4. JS typeOf Operator\n"
+"5. JS Debugging\n"
+"6. JS Local Storage\n"
+"7. JS Callback\n"
+"8. JS Closures\n"
+"9. JS defer\n"
+"10. JS scope\n"
+"11. JS Void\n"
+),
+
+];
